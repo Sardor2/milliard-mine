14582d45a82364463e48efc2f8c6f60dd71a222e
sardorX <abdurakhimov.sardor@gmail.com>
2021-10-27 17:15:17 +0500

    Added fonts and business-section

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -2,8 +2,11 @@ import * as React from "react";
 
 import "./styles.scss";
 
-const Container = ({ children, className, ...props }) => (
-  <div className={`container-wrapper ${className}`} style={props}>
+const Container = ({ children, className, maxWidth = "lg", ...props }) => (
+  <div
+    className={`container-wrapper container-wrapper-${maxWidth} ${className}`}
+    style={props}
+  >
     {children}
   </div>
 );
--- src/components/global-wrapper/index.js
+++ src/components/global-wrapper/index.js
@@ -4,7 +4,6 @@ const styles = {
   display: "flex",
   flexDirection: "column",
   height: "100vh",
-  overflow: "hidden",
 };
 
 const GlobalWrapper = ({ children }) => {
