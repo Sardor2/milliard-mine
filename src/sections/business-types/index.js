5101a041587ccd8db32f1d6a9f69aa35fbedf133
sardorX <abdurakhimov.sardor@gmail.com>
2021-10-28 20:11:07 +0500

    Gave constant widht/height business-card

--- src/sections/business-types/index.js
+++ src/sections/business-types/index.js
@@ -4,25 +4,24 @@ import Container from "../../components/container";
 import Flex from "../../components/flex";
 import BusinessCard from "../../components/business-card";
 import wallet from "../../images/wallet.svg";
+import swap from "../../images/swap.svg";
 
 const BusinessTypes = () => {
   return (
     <section className={"business-types"} id={"business-types"}>
       <Container maxWidth={"md"}>
         <h2>Business turlari</h2>
-        <div className="scroller">
-          <Flex className={"corousel"}>
-            {Array(10)
-              .fill(1)
-              .map((item, i) => (
-                <BusinessCard
-                  className={"card"}
-                  key={i}
-                  img={wallet}
-                  name={"Savod sotiq"}
-                />
-              ))}
-          </Flex>
+        <div className="scroller flex">
+          {Array(10)
+            .fill(1)
+            .map((item, i) => (
+              <BusinessCard
+                className={"card"}
+                key={i}
+                img={swap}
+                name={"Savod sotiq"}
+              />
+            ))}
         </div>
       </Container>
     </section>
