194393fda165ea745cab736b7c306bab14bb3bd0
sardorX <abdurakhimov.sardor@gmail.com>
2021-11-14 14:36:06 +0500

    Created dynamic businesses page

---
+++ src/pages/businesses/[slug].js
@@ -0,0 +1,56 @@
+import React from "react";
+import MainLayout from "../../layouts/main-layout";
+import logo from "../../images/logo.svg";
+import Header from "../../components/header";
+import BusinessCard from "../../components/business-card";
+import swap from "../../images/swap.svg";
+import "./styles.scss";
+import Contact from "../../sections/contact";
+import { BUSINESSES } from "../../constants";
+import { useTranslation } from "react-i18next";
+import Spinner from "../../components/spinner";
+import PageSpinner from "../../components/page-spinner";
+import { useBusinesses } from "../../services/use-businesses";
+import { Redirect } from "@reach/router";
+
+const BusinessesPage = ({ params }) => {
+  const { t } = useTranslation();
+  const { loading, data, error } = useBusinesses(params.slug);
+
+  if (error) {
+    return <h1>{JSON.stringify(error)}</h1>;
+  }
+
+  console.log(data);
+
+  // console.log(params.slug);
+  return (
+    <PageSpinner loading={loading}>
+      <MainLayout>
+        <Header>
+          <div className="flex justify-center">
+            <img className="w-40" src={logo} alt="" />
+          </div>
+          <section id="businesses" className="sm:p-10 md:p-20 lg:p-40">
+            <h2 className="text-center capitalize mt-20 lg:mt-2">
+              {t("businesses")}
+            </h2>
+            <div className="business-cards justify-between flex mt-20 flex-wrap m-auto">
+              {data?.map((item) => (
+                <BusinessCard
+                  className="mb-10"
+                  key={item.id}
+                  img={item.logo_url}
+                  name={item.name}
+                />
+              ))}
+            </div>
+          </section>
+          <Contact />
+        </Header>
+      </MainLayout>
+    </PageSpinner>
+  );
+};
+
+export default BusinessesPage;
