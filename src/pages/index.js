e1722250be41d2181d08c3fb762f399c56ae125e
Vip Abdujamilov <vip.abdujamilov@gmail.com>
2021-12-02 19:33:52 +0500

    conflict fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,32 +58,8 @@ const HomePage = () => {
         <Partners partners={data?.partners} />
         <Statistics tags={data?.tags} />
         <Contact />
-<<<<<<< HEAD
-        <button onClick={() => setModalOpen(true)} className="btn">
-          Click me
-        </button>
-        <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-          <div className="lang-modal">
-            <AnimText title="CHoose language CHoose language CHoose language CHoose language CHoose language " />
-            <div className="lang-teaser">
-              <h3>Qaysi til sizga ma’qul?</h3>
-              <div className="lang-select">
-                <div onClick={handleClick} id="uz" className="lang-item">
-                  O'zbek
-                </div>
-                <div onClick={handleClick} id="en" className="lang-item">
-                  English
-                </div>
-                <div onClick={handleClick} id="ru" className="lang-item">
-                  Русский
-                </div>
-              </div>
-            </div>
-          </div>
-=======
         <Modal open={shouldRenderLanguageBanner}>
           <LanguageBanner />
->>>>>>> main
         </Modal>
       </MainLayout>
     </PageSpinner>
